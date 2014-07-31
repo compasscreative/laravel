6eb3a672e3b38ca7b71b2d992cc88cdded910c11
Jonathan Reinink <jonathan@reinink.ca>
2014-07-31 10:31:00 -0400

    Add a few default JavaScript libraries.

---
+++ public/js/all.js
@@ -0,0 +1,10 @@
+/*
+@codekit-prepend '../vendor/jquery/jquery.js'
+@codekit-prepend '../vendor/jquery-validation/jquery.validate.js'
+*/
+
+$(function() {
+
+    $('.contact_form').validate();
+
+});
