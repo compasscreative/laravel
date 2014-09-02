cb03c76fad2cfa532fa030346a83a9e525c7f67c
Jonathan Reinink <jonathan@reinink.ca>
2014-09-02 16:07:51 -0400

    Improve Gulp configuration (add Plumber).

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var gulp = require('gulp')
 var include = require('gulp-include');
 var livereload = require('gulp-livereload');
 var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var sass = require('gulp-ruby-sass');
 var uglify = require('gulp-uglify');
@@ -22,11 +23,12 @@ var uglify = require('gulp-uglify');
 
 gulp.task('css', function () {
     return gulp.src('public/sass/all.scss')
-               .pipe(sass({ style: 'compressed' }))
-               .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-               .pipe(gulp.dest('public/css'))
-               .pipe(livereload({ auto: false }))
-               .pipe(notify({ message: 'Compiled CSS (<%=file.relative%>)' }));
+        .pipe(plumber())
+        .pipe(sass({ style: 'compressed' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('public/css'))
+        .pipe(livereload({ auto: false }))
+        .pipe(notify({ message: 'Compiled CSS (<%=file.relative%>)' }));
 });
 
 
@@ -38,34 +40,46 @@ gulp.task('css', function () {
 
 gulp.task('js', function() {
     return gulp.src('public/js/all.js')
-               .pipe(include())
-               .pipe(uglify())
-               .pipe(rename('all.min.js'))
-               .pipe(gulp.dest('public/js'))
-               .pipe(notify({ message: 'Minified JS (<%=file.relative%>)' }));
+        .pipe(plumber())
+        .pipe(include())
+        .pipe(uglify())
+        .pipe(rename('all.min.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(notify({ message: 'Minified JS (<%=file.relative%>)' }));
 });
 
 
 /*
 |--------------------------------------------------------------------------
-| Setup watcher and LiveReload
+| Setup LiveReload and watcher
 |--------------------------------------------------------------------------
 */
 
 gulp.task('default', function () {
 
-    // Watch .scss files
-    gulp.watch('public/sass/**/*.scss', ['css']);
-
-    // Watch .js files
-    gulp.watch('public/js/*.js', ['js']);
-
     // Create LiveReload server
     var server = livereload();
     livereload.listen();
 
-    // Reload on view change
-    gulp.watch(['app/views/**']).on('change', function (file) {
+    // Watch .scss files
+    gulp.watch([
+        'public/sass/*.scss',
+        'public/sass/**/*.scss'
+    ], function (file) {
+        gulp.run('css');
+    })
+
+    // Watch .js files
+    gulp.watch([
+        'public/js/*.js'
+    ], function (file) {
+        gulp.run('js');
+    })
+
+    // Watch view files
+    gulp.watch([
+        'app/views/**'
+    ], function (file) {
         server.changed(file.path);
-    });
+    })
 });
